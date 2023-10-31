b918059ab9a3442082c32217ad6df068b01f0dda
Ryan Gogats <ryangogats@ryans-mbp.mynetworksettings.com>
2023-10-31 09:27:18 -0400

    setup initial HTML elements and grid selectors

---
+++ script.js
@@ -0,0 +1,10 @@
+// const gridSquare = document.createElement("div");
+const gridContainer = document.querySelector("div.container");
+const heightInput = document.querySelector("input#height");
+const widthInput = document.querySelector("input#width");
+
+console.log("height", heightInput.value);
+
+for (i = 0; i < heightInput.value; i++) {
+    null;    
+}
